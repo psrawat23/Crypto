862628a7110cf3b03254e39b5db9e2bccdb7e28e
purushottam singh <78137573+psrawat23@users.noreply.github.com>
2022-04-08 01:21:27 +0530

    Revert "added crpto code"

--- blockchain1/urls.py
+++ blockchain1/urls.py
@@ -5,9 +5,5 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('mine_block/',views.mine_block,name="mine_block"),
     path('get_chain/',views.get_chain,name='get_chain'),
-    path('is_valid/',views.is_valid,name='is_valid'),
-    path('connect_node/',views.connect_node,name="connect_node"),
-    path('replace_chain/',views.replace_chain,name='replace_chain'),
-    path('add_transaction/',views.add_transaction,name='add_transaction'),
-
+    path('is_valid/',views.is_valid,name='is_valid')
 ]
